6f9206cb24bb32fe0174498d6024f52c2c8d4053
ljb100 <izb2790@gmail.com>
2023-08-29 16:29:39 +0900

    P_13699.cpp

---
+++ DynamicProgramming/P_13699.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+// 문제주소 : https://www.acmicpc.net/problem/13699
+// 메모리, 응답시간 : 2020kb, 0ms
+
+long long* array;
+int input;
+long long t(int n) {
+	if (n == 0) return 1;
+	if (array[n] != 0) return array[n];
+	for(int i = 1; i <= n; i++)
+		array[n] += ( t(i-1) * t(n - i) );
+	return array[n];
+}
+
+int main() {
+	std::cin >> input;
+
+	array = new long long[input+1];
+	std::fill(&array[0], &array[input + 1], 0);
+	std::cout << t(input);
+	return 0;
+}
